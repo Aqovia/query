5d103a79a816b5b9f6e2e699df0745c6696f001f
Ricky Attilia <rattilia@digitalrealty.com>
2023-12-09 20:24:24 +0000

    Started implementation of Lit adapter

---
+++ packages/lit-query/src/index.ts
@@ -0,0 +1,7 @@
+export * from '@tanstack/query-core'
+
+export { ObserverController } from './observerController'
+export { provideQueryClient } from './provideQueryClient'
+export { queryClientContext } from './queryClientContext'
+export { useMutation } from './useMutation'
+export { useQuery } from './useQuery'
